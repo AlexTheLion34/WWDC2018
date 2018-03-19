2c6e94b184ed33c2840a79fb66c6a6a9dde18589
Alexey Petrenko <AlexTheLion@MacBook-AirAleksej.local>
2018-03-19 19:47:28 +0300

    Added gradient

--- Sources/FirstViewController.swift
+++ Sources/FirstViewController.swift
@@ -1,14 +1,28 @@
 import UIKit
 
 public class FirstViewController: UIViewController {
+    
+    let gradientColors = [#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1).cgColor, #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1).cgColor, #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1).cgColor, #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1).cgColor, #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1).cgColor, #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1).cgColor]
+    
+    var gradientLayer: CAGradientLayer! {
+        didSet {
+            gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 1, y: 0)
+            gradientLayer.colors = gradientColors
+        }
+    }
+    
+    public override func viewDidLayoutSubviews() {
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-        let label = UILabel(frame: CGRect(x: 100, y: 200, width: 100, height: 40))
-        label.layer.cornerRadius = 5
-        label.layer.masksToBounds = true
-        label.layer.borderWidth = 1
-        label.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        view.addSubview(label)
+        gradientLayer = CAGradientLayer()
+        view.layer.insertSublayer(gradientLayer, at: 0)
+         let imageView = UIImageView(frame: CGRect(x: 50, y: 100, width: 150, height: 150))
+         imageView.image = UIImage(named: "Apple.png")
+         view.addSubview(imageView)
     }
+    
 }
