8f22dca9e818abb9d6af1e87aedc54200b35c4aa
Alexey Petrenko <AlexTheLion@MacBook-AirAleksej.local>
2018-03-20 02:41:24 +0300

    Added some animation

--- Sources/FirstViewController.swift
+++ Sources/FirstViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-public class FirstViewController: UIViewController {
+public class FirstViewController: UIViewController, CAAnimationDelegate {
     
     let gradientColors = [#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1).cgColor, #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1).cgColor, #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1).cgColor, #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1).cgColor, #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1).cgColor, #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1).cgColor]
     
@@ -12,17 +12,79 @@ public class FirstViewController: UIViewController {
         }
     }
     
+    var shapeLayer: CAShapeLayer! {
+        didSet {
+            let color = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+            shapeLayer.lineWidth = 10
+            shapeLayer.lineCap = "round"
+            shapeLayer.fillColor = nil
+            shapeLayer.strokeEnd = 1
+            shapeLayer.strokeColor = color
+        }
+    }
+    
+    var overShapeLayer: CAShapeLayer! {
+        didSet {
+            let color = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+            overShapeLayer.lineWidth = 10
+            overShapeLayer.lineCap = "round"
+            overShapeLayer.fillColor = nil
+            overShapeLayer.strokeEnd = 0
+            overShapeLayer.strokeColor = color
+        }
+    }
+    
+    func setupShapeLayer(_ shapeLayer: CAShapeLayer) {
+        shapeLayer.frame = view.bounds
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: self.view.frame.width / 2 - 100, y: self.view.frame.height / 4 + 300))
+        path.addLine(to: CGPoint(x: self.view.frame.width / 2 + 100, y: self.view.frame.height / 4 + 300))
+        shapeLayer.path = path.cgPath
+    }
+    
     public override func viewDidLayoutSubviews() {
         gradientLayer.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+        setupShapeLayer(shapeLayer)
+        setupShapeLayer(overShapeLayer)
     }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
         gradientLayer = CAGradientLayer()
+        shapeLayer = CAShapeLayer()
+        overShapeLayer = CAShapeLayer()
+        
         view.layer.insertSublayer(gradientLayer, at: 0)
-         let imageView = UIImageView(frame: CGRect(x: 50, y: 100, width: 150, height: 150))
-         imageView.image = UIImage(named: "Apple.png")
-         view.addSubview(imageView)
+        view.layer.addSublayer(shapeLayer)
+        view.layer.addSublayer(overShapeLayer)
+        
+        let imageView = UIImageView(frame: CGRect(x: 95, y: 20, width: 100, height: 100))
+        imageView.image = UIImage(named: "Apple.png")
+        view.addSubview(imageView)
+        
+        let button = UIButton(frame: CGRect(x: 100, y: 325, width: 100, height: 50))
+        button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        button.layer.cornerRadius = 8
+        button.layer.masksToBounds = true
+        button.layer.borderWidth = 2
+        button.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        button.alpha = 0.8
+        
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        view.addSubview(button)
+    }
+    
+    @objc func buttonAction() {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.toValue = 1
+        animation.duration = 3
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        animation.fillMode = kCAFillModeBoth
+        animation.isRemovedOnCompletion = true
+        animation.delegate = self
+        overShapeLayer.add(animation, forKey: nil)
+        //need to add completion block
     }
     
 }
